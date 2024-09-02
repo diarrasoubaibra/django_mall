715c0d48a872569b09d25e5607b94ec24a9e5a64
Diarrassouba ibrahim <diarrassoubaibra58@gmail.com>
2024-09-02 15:37:47 +0000

    cmmit du 02/09/2024

--- carts/views.py
+++ carts/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 # Create your views here.
 
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
-        cart = request.session.Create()
+        cart = request.session.create()
     return cart
 
 
@@ -56,6 +57,8 @@ def remove_cart_item(request, product_id):
 
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
+        tax = 0
+        grand_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
@@ -63,7 +66,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2 * total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
 
     context = {
